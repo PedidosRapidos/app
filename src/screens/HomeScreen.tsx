81d127b07d140be69838d198f12c1eb3ba66d516
AgustinLeguizamon <agustinmleguizamon@gmail.com>
2022-05-08 16:11:54 -0300

    added dummy get request on search

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,7 +3,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { globalStyles } from "../res/globalStyles";
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { SearchBar } from "../ui/components/SearchBar";
-import { useState } from "react";
+import React, { useState } from "react";
 import { Title } from "../ui/components/Title";
 import { spacing } from "../res/spacing";
 import { MediumTypography, normalizeSize, Typography } from "../res/typography";
@@ -12,10 +12,33 @@ import { SectionTitle } from "../ui/components/SectionTitle";
 import { SectionContainer } from '../ui/components/SectionContainer';
 import { PRImage } from '../ui/components/PRImage';
 import { imageStyles } from "../res/imageStyles";
+import { MainButton } from "../ui/components/MainButton";
+import { executeGetRequest } from "../services/executeGetRequest";
+import { Loader } from "../ui/components/Loader";
 
 export const HomeScreen = () => {
   const [searchValue, setSearchValue] = useState("");
-  
+  const [isLoading, setIsLoading] = useState(false);
+
+  const searchProducts = async () => {
+    setIsLoading(true);
+    try {
+      const respLogin = await executeGetRequest();
+    } catch (err: any) {
+      if (
+        err.code == "auth/user-not-found" ||
+        err.code == "auth/wrong-password"
+      ) {
+        console.log("ERROR: Los datos son incorrectos");
+      } else {
+        console.log(err.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+
+  }
+
   return (
     <SafeAreaView style={globalStyles.generalContainer}>
       <KeyboardAwareScrollView
@@ -31,15 +54,23 @@ export const HomeScreen = () => {
             value={searchValue}
             placeholder="Search product name"
           />
+          <MainButton
+              text="Search"
+              onPress={() => {
+                searchProducts();
+              }}
+              backgroundColor={colors.orange}
+            />
       </SectionContainer>
-
       <SectionContainer>
         <SectionTitle text="Categories"/>
         <View style={styles.row}>
+          {/*TODO: Convertir en un componente CategorieIcon*/}
           <View>
             <Image source={require("../res/img/logo.png")} style={imageStyles.categorieIcon}></Image>
             <Typography>Food</Typography>
           </View>
+          {/*TODO: Convertir en un componente CategorieIcon*/}
           <View>
             <Image source={require("../res/img/logo.png")} style={imageStyles.categorieIcon}></Image>
             <Typography>Products</Typography>
@@ -48,6 +79,7 @@ export const HomeScreen = () => {
       </SectionContainer>
 
       </KeyboardAwareScrollView>
+      <Loader visible={isLoading} />
     </SafeAreaView>
   );
 };
