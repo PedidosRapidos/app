1d8aa3db0da98db48ec2df6fc52586a227587b95
Paulo Cesar Cuneo <cuneo.paulo.cesar@gmail.com>
2022-05-10 20:50:23 -0300

    hack image cache

--- src/ui/components/ProductPreview.tsx
+++ src/ui/components/ProductPreview.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { KeyboardTypeOptions, StyleSheet, TextInput, Image, View, Text } from "react-native";
+import {
+  KeyboardTypeOptions,
+  StyleSheet,
+  TextInput,
+  Image,
+  View,
+  Text,
+} from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 import { colors, colorWithOpacity } from "../../res/colors";
 import { imageStyles } from "../../res/imageStyles";
@@ -7,23 +14,25 @@ import { spacing } from "../../res/spacing";
 import { normalizeSize, Typography } from "../../res/typography";
 import { Title } from "./Title";
 
-import {API_URL} from "../../services/config"
+import { API_URL } from "../../services/config";
 interface Props {
-    product: any;
-  }
-  
-
-export const ProductPreview = ({product} : Props) => {
-    return (
-        <View>
-            <Image source={{uri:`${API_URL}/products/${product.id}/image`}} style={imageStyles.categorieIcon}></Image>
-            <Typography>{product.name}</Typography>
-            <Typography>{product.price}</Typography>
-        </View>
-    )
-
+  product: any;
 }
 
-const styles = StyleSheet.create({
+export const ProductPreview = ({ product }: Props) => {
+  console.log(`${API_URL}/products/${product.id}/image`);
+  return (
+    <View>
+      <Image
+        source={{
+          uri: `${API_URL}/products/${product.id}/image?q=${new Date()}`,
+        }}
+        style={{ ...imageStyles.categorieIcon, width: 305, height: 159 }}
+      ></Image>
+      <Typography>{product.name}</Typography>
+      <Typography>{product.price}</Typography>
+    </View>
+  );
+};
 
-});
+const styles = StyleSheet.create({});
