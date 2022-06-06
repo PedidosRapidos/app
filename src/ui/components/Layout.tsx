55e61d46b20b70f522e31ac929b35768236e47fe
Paulo Cesar Cuneo <cuneo.paulo.cesar@gmail.com>
2022-06-06 20:16:35 -0300

    minor layout improvements

--- src/ui/components/Layout.tsx
+++ src/ui/components/Layout.tsx
@@ -1,42 +1,33 @@
 import React, { PropsWithChildren } from "react";
-import { StyleProp, View, ViewStyle } from "react-native";
+import { StyleProp, View, ViewStyle, StyleSheet } from "react-native";
 
-export const Row = ({
-  children,
-  style,
-}: PropsWithChildren<{ style?: StyleProp<ViewStyle> }>) => {
-  return (
-    <View
-      style={[
-        {
-          flexDirection: "row",
-          alignItems: "center",
-          width: "100%",
-        },
-        style,
-      ]}
-    >
-      {children}
-    </View>
-  );
-};
+export const Row = React.memo(
+  ({
+    children,
+    style,
+  }: PropsWithChildren<{ style?: StyleProp<ViewStyle> }>) => {
+    return <View style={[styles.row, style]}>{children}</View>;
+  }
+);
 
-export const Column = ({
-  children,
-  style,
-}: PropsWithChildren<{ style?: StyleProp<ViewStyle> }>) => {
-  return (
-    <View
-      style={[
-        {
-          flexDirection: "column",
-          alignItems: "flex-start",
-          justifyContent: "center",
-        },
-        style,
-      ]}
-    >
-      {children}
-    </View>
-  );
-};
+export const Column = React.memo(
+  ({
+    children,
+    style,
+  }: PropsWithChildren<{ style?: StyleProp<ViewStyle> }>) => {
+    return <View style={[styles.column, style]}>{children}</View>;
+  }
+);
+
+const styles = StyleSheet.create({
+  column: {
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+  },
+});
